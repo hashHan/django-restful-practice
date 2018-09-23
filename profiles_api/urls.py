31a6e8aed83a92a8b9138f0ddb368ca3495e2aa0
Haeseong <haeseong.han@kono.ai>
2018-09-23 18:49:06 +1000

    Start APIview

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.conf.urls import include
-from django.contrib import admin
+
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('profiles_api.urls')),
+    url(r'^hello-view/', views.HelloApiView.as_view()),
+    
 ]
